975fec467d48b1c6955e0e5f664ab3c049086b20
cengizelif <cenelif@gmail.com>
2023-03-31 08:39:11 +0300

    Add project files.

---
+++ MVCUygulama_30032023/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCUygulama_30032023.MvcApplication" Language="C#" %>
---
+++ MVCUygulama_30032023/Library/HelperClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace MVCUygulama_30032023.Library
+{
+    public static class HelperClass
+    {
+        public static MvcHtmlString Button(this HtmlHelper helper, string name="", string text="", string id="",ButonTip typ=ButonTip.button)
+        {
+            string html =string.Format("<button type='{0}' id='{1}' name='{2}'> {3} </button>",typ,id,name,text);
+            return MvcHtmlString.Create(html);
+        }
+        public static MvcHtmlString Button(this HtmlHelper helper,  string text = "", string id = "", string name = "",string cssclass= "btn", ButonTip typ = ButonTip.button)
+        {
+            TagBuilder tag = new TagBuilder("button");
+            tag.GenerateId(id);
+            tag.Attributes.Add(new KeyValuePair<string,string>("type",typ.ToString()));
+            tag.Attributes.Add(new KeyValuePair<string, string>("name", name));
+            tag.AddCssClass(cssclass);
+            
+            tag.SetInnerText(text);
+
+            return MvcHtmlString.Create(tag.ToString());
+        }
+
+    }
+
+    public enum ButonTip
+    {
+        button,reset,submit
+    }
+}
